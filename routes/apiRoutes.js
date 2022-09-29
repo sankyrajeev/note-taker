fb7e3390e6bfaba3cc4017b0e7e71ec52c344f60
Vontell <sankarsh.rajeev@gmail.com>
2022-09-29 15:51:02 -0700

    uploaded files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const { application } = require("express");
+const uuid = require('../helpers/uuid.js');
+const  { readFromFile, writeToFile, readAndAppend } = require("../helpers/fsUtils.js")
+
+
+router.get('/notes',(req,res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+router.post('/notes', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title,text} = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+       
+        title,
+        text,
+        id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting Note');
+    }
+  });
+
+
+  router.delete('/notes/:id', (req, res) => {
+    note
+      .removeNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch((err) => res.status(500).json(err));
+  });
+  
+  
+
+  module.exports = router;
+  
